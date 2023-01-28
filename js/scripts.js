6aaceaa0b7c13d13adf5fdfdc712dbaef2423656
ifaxec <xecfahim.ahmed@gmail.com>
2023-01-28 14:00:57 +0000

    updated script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 let employeesList = fetch('https://randomuser.me/api/?results=12')
     .then(response => response.json())
-    .then(result   => console.log(result))
+    .then(result => console.log(result))
+    .catch(error => console.error("Error:", error))
 
-    console.log(employeesList);
+    //console.log(employeesList);
 
     
