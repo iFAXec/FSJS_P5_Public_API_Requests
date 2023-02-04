49801a77a05066f803aeeb07e64cf9aa93819c05
ifaxec <xecfahim.ahmed@gmail.com>
2023-02-04 16:09:36 +0000

    updated script.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,26 +5,30 @@ let employees = [];
 
 
 fetch(url)
-    .then(response => response.json())
-    //.then(result => console.log(result))        
+    .then(checkStatus)
+    .then(response => response.json())              
     .then(data => {
         employees = data.results;
-        employees.map((item, index) => generateHTML(item, index));
+        console.log(employees);
+        employees.map((item, index) => displayCard(item, index));
     })    
     //.catch(error => console.error("Error:", error))
 
+    function checkStatus(response) {
+        if (response && response.ok) {
+            return response;              
+        } 
+    }
     
 
 
     //Helper function which generate html within the card
-    function generateHTML(item, index) {
+    function displayCard(item, index) {
             const img = item.picture.large;
             const name = item.name;
             const email = item.email;
             const city = item.location.city;
-            const state = item.location.state;            
-
-             
+            const state = item.location.state;          
 
             const html = `
             <div class="card" data-index=${index}> 
@@ -42,9 +46,11 @@ fetch(url)
             //console.log(html);
             galleryDiv.insertAdjacentHTML("beforeend", html);        
     }
-    function emptyModal() {       
+
+
+    function createModal() {
         const htmlModal = `
-        <div class="modal-container data-index="" style = "display:none">
+             <div class="modal-container data-index="" style="display=none">
                 <div class="modal">
                     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                     <div class="modal-info-container">
@@ -68,42 +74,65 @@ fetch(url)
         galleryDiv.insertAdjacentHTML("afterend", htmlModal);
     }
 
+    createModal();
 
-    function updateModal(index) {
+function generateModal (){
+    const modalImg = document.querySelector(".modal-img");
+    const modalName = document.querySelector(".modal-name");
+    const modalEmail = document.querySelector(".modal-text");         
+    const modalCity = document.querySelectorAll(".modal-text.cap")[0];
+    const modalCell = document.querySelectorAll(".modal-text")[2];  
+    const modalAdress = document.querySelectorAll(".modal-text")[3];  
+    const modalBirthday = document.querySelectorAll(".modal-text")[4];        
+    console.log(modalBirthday);
+    
 
-        //const {name: {first, last}, cell, email, dob: {date}, location:{street, city, state,country, postcode}, picture  } = employees[index];
+    modalImg.src = employee.picture.large;
+    modalImg.alt = `Profile picture of ${employee.name.first} ${employee.name.last}`;    
+    modalName.textContent = `${employee.name.first} ${employee.name.last}`;
+    modalEmail.textContent = `${employee.email}`;
+    modalCity.textContent = `${employee.location.city}`;
+    modalCell.textContent = `${employee.cell}`;
+    modalAdress.textContent = `${employee.location.street.number} ${employee.location.street.name}, ${employee.location.state}, ${employee.location.postcode}`;
+    modalBirthday.textContent = `Birthday: ${employee.dob.date}`;
 
-        let birthday = new Date(dob);
-        birthday.toLocaleDateString("en-GB");         
-        
-    }
+    
 
-updateModal();
+
+}
 
 
 
-document.addEventListener("click", (e)=>{
+
+
+
+
+
+
+
+
+const modalContainer = document.querySelector(".modal-container");
+const closeModal = document.getElementById("modal-close-btn");
+
+  closeModal.addEventListener("click", ()=>{
+    modalContainer.style.display = "none";    
+  });    
+
+    function displayModal(){   
+    modalContainer.style.display = "block";   
+    generateModal()    
+}
+  console.log(modalContainer);
+
+
+
+galleryDiv.addEventListener("click", (e)=>{
         const target = e.target.closest(".card");
         const index = target.getAttribute("data-index");  
-        console.log(index);
-        employee = employees[index];
-        console.log(employee);     
-        updateModal(employee);
+        employee = employees[index];        
+        //console.log(index);
+        console.log(employee);             
+        displayModal();
         
-    })  
-
-    
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
+        
+    });
